471ba47adcb6fce72e0987b6a490b873622b0fdb
minhajul islam <mm01882390860@gmail.com>
2026-03-12 19:23:17 +0600

    Add navigation link

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { Outlet } from 'react-router'
+import { Link, Outlet } from 'react-router'
 
 const Home = () => {
   return (
     <div>
       <h1>স্বাগতম আমার Website এ</h1>
+      <Link to="/about" >About</Link>
+      <Link to="/contact" >Contact</Link>
+      <Link to="/team/dhaka" >Team Dhaka</Link>
       <Outlet />
     </div>
   )
