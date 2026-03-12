789c4b4de9a9ca62c30ca592cd7f410204af38a8
minhajul islam <mm01882390860@gmail.com>
2026-03-12 19:41:33 +0600

    Implement useNeviget

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router'
+import { Link, Outlet, useNavigate } from 'react-router'
 
 const Home = () => {
+            const naviget = useNavigate()
+
+            function handleClick() {
+            naviget("/about")
+            
+          }
+
   return (
     <div>
       <h1>স্বাগতম আমার Website এ</h1>
       <Link to="/about" >About</Link>
       <Link to="/contact" >Contact</Link>
       <Link to="/team/dhaka" >Team Dhaka</Link>
+     
+
+      <button className='btn bg-primary' onClick={handleClick}>About</button>
       <Outlet />
     </div>
   )
